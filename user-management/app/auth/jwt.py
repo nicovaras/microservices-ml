ad8382e187f500c9ef3fd445c1059c7850fc68e9
Your Name <you@example.com>
2024-09-30 21:06:01 +0200

    auth and jwt

---
+++ user-management/app/auth/jwt.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+
+load_dotenv()  # Load environment variables from .env
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
