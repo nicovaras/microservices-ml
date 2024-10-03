444b16e82750970fed1b71fc8bb0ee9c4a40a1a1
Your Name <you@example.com>
2024-10-03 21:20:19 +0200

    Comunication between microservices

--- data-service/app/routers/prediction.py
+++ data-service/app/routers/prediction.py
@@ -5,11 +5,12 @@ from app.schemas.prediction import PredictionCreate, PredictionResponse
 from app.database.database import get_db
 from typing import List, Optional
 import json
+from auth import get_current_user
 
 router = APIRouter()
 
 @router.post("/save", response_model=PredictionResponse)
-def save_prediction(prediction: PredictionCreate, db: Session = Depends(get_db)):
+def save_prediction(prediction: PredictionCreate, db: Session = Depends(get_db), user=Depends(get_current_user)):
     db_prediction = PredictionRecord(
         user_id=prediction.user_id,
         input_data=json.dumps(prediction.input_data),
@@ -26,7 +27,7 @@ def save_prediction(prediction: PredictionCreate, db: Session = Depends(get_db))
     return db_prediction
 
 @router.get("/history/{user_id}", response_model=List[PredictionResponse])
-def get_user_predictions(user_id: int, db: Session = Depends(get_db)):
+def get_user_predictions(user_id: int, db: Session = Depends(get_db), user=Depends(get_current_user)):
     predictions = db.query(PredictionRecord).filter(PredictionRecord.user_id == user_id).all()
     for prediction in predictions:
         prediction.input_data = json.loads(prediction.input_data)
